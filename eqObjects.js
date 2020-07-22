73a55b9e2bf84211fa050171a73fb59a5a0af065
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-22 19:01:58 +0000

    Adding func to lotide to allow for the comparing of objects

---
+++ eqObjects.js
@@ -0,0 +1,60 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❎| Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌| Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arrayOne, arrayTwo) => {
+  //Start off by checking if arrays are of equal length
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+
+  for (let i = 0; i <= arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+const eqObjects = (obj1, obj2) => {
+  let obj1Keys = Object.keys(obj1);
+  let obj2Keys = Object.keys(obj2);
+
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+  for (let item of obj1Keys) {
+    if (obj1[item] !== obj2[item] && Array.isArray(obj1[item]) === false) {
+      return false;
+    }
+    
+    if (Array.isArray(obj1[item])) {
+      if (eqArrays(obj1[item], obj2[item]) === false) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
