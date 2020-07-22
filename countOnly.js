b0d44e2c899363c42fa8b1c80a8cfc6d907a9a76
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-22 17:34:04 +0000

    Adding count only to lotdie

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❎| Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌| Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let countedItems = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (countedItems[item]) {
+        countedItems[item] += 1;
+      } else {
+        countedItems[item] = 1;
+      }
+    }
+  }
+  return countedItems;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+
+ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+console.log(result1)
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
