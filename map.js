9a0dde04e437352229eb1b280aa628deb8e2a138
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-23 18:06:28 +0000

    adding our own version of a map function to lotdie

---
+++ map.js
@@ -0,0 +1,12 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(inputArray, callbackFunc) {
+  const results = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    results.push(callbackFunc(inputArray[i]));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
