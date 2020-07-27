8eea69ec270af8bea5d99e20654a735c196b3da8
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-27 17:23:15 +0000

    adding test files for assertEqual and head / exporting both functions

--- assertEqual.js
+++ assertEqual.js
@@ -7,12 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "1");
-assertEqual("cat", 4);
-assertEqual("cat", "dog");
-assertEqual("cat", "cat");
-assertEqual(4, 4);
-assertEqual(4, 5);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual')
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, "1");
+assertEqual("cat", 4);
+assertEqual("cat", "dog");
+assertEqual("cat", "cat");
+assertEqual(4, 4);
+assertEqual(4, 5);
