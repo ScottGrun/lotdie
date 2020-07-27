01f13f95e63bff8922821489bea302323d7a27b3
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-27 18:43:19 +0000

    Adding mocha and chai testing to middle and tail funcs

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,12 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const middle = require("../middle");
+const { expect } = require("chai");
 
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+describe("#middle", () => {
+  it("Should return a single element if the array is odd", () => {
+    expect(middle([1, 2, 3])).to.deep.equal([2]);
+  });
+
+  it("Should return the two middle elements if even", () => {
+    expect(middle([1, 2, 3, 4])).to.deep.equal([2, 3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const tail = require("../tail");
+const { expect } = require("chai");
 
+describe("#tail", () => {
+  it("Should return an array minus the first element", () => {
+    let testArr = ["Yo Yo", "Lighthouse", "Labs"];
+    expect(tail(testArr)).to.deep.equal(["Lighthouse", "Labs"]);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3);
-assertEqual(tail(words).length, 2);
+  it("Should mutate the input array", () => {
+    let testArr = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(testArr);
+    expect(testArr.length).to.equal(3);
+  });
+});
+
+// no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+// assertEqual(tail(words)., 2);
